8d36e0c6d7e88371506dc5720fe23602f54ac82f
krisbock <krbock@microsoft.com>
2021-11-09 09:14:18 +1000

    update library_install

--- databricks/init_scripts/library_install.sh
+++ databricks/init_scripts/library_install.sh
@@ -13,6 +13,7 @@ source /databricks/spark/conf/spark-env.sh
 /databricks/python/bin/pip install -U hail==$HAIL_VERSION
 hail_jar_path=$(find /databricks/python3 -name 'hail-all-spark.jar')
 cp $hail_jar_path /databricks/jars
+export hail_jar_path="/databricks/jars/hail-all-spark.jar"
 
 # Note: This configuration takes precedence since configurations are
 # applied in reverse-lexicographic order.
@@ -20,5 +21,6 @@ cat <<HERE >/databricks/driver/conf/00-hail.conf
 [driver] {
   "spark.kryo.registrator" = "is.hail.kryo.HailKryoRegistrator"
   "spark.serializer" = "org.apache.spark.serializer.KryoSerializer"
+  "spark.jars" = "/databricks/jars/hail-all-spark.jar""
 }
 HERE
